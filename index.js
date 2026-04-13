012b738d240186d69508e34227c5a6a54aa51dcb
Alphine <alphinemkorea3@gmail.com>
2026-04-13 14:12:35 +0300

    finished js array merge assignments

---
+++ index.js
@@ -0,0 +1,17 @@
+const group1 = ["John", "Mary"];
+const group2 = ["Alex", "Jane"];
+const group3 = ["Mike"];
+
+function combineUsers(...arrays) {
+  let result = [];
+
+  for (let arr of arrays) {
+    result = result.concat(arr);
+  }
+
+  return result;
+}
+
+const combined = combineUsers(group1, group2, group3);
+
+console.log(combined);
